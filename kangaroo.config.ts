f18f96c849d53ed0bc635a12f54bb3ba69000289
Soushi888 <sacha.pignot@protonmail.com>
2025-09-17 07:43:51 +0200

    bump version to 0.1.0-alpha.7

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   // Alpha 6 release with comprehensive exchanges system and Effect-first architecture
   appId: 'requests-and-offers.happenings-community.kangaroo-electron',
   productName: 'Requests and Offers',
-  version: '0.1.0-alpha.6',
+  version: '0.1.0-alpha.7',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
